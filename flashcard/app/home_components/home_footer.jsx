5cb079fd6408e0c7c30023a05ef7533230f7f50d
Lesvan Chavez <lesvanrealm@gmail.com>
2025-07-30 17:10:43 -0500

    Home Page Foundation
    Started to work on Home page components and the structuring of the home page.

---
+++ flashcard/app/home_components/home_footer.jsx
@@ -0,0 +1,30 @@
+
+
+
+
+export default function HomeFooter() {
+    return (
+        <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content grid-rows p-10">
+  <nav>
+    <h6 className="footer-title">Aaron Frnklin</h6>
+    <a className="link link-hover">Contact</a>
+    <a className="link link-hover">Design</a>
+    <a className="link link-hover">Marketing</a>
+    <a className="link link-hover">Advertisement</a>
+  </nav>
+  <nav>
+    <h6 className="footer-title">Lesvan Chavez</h6>
+    <a className="link link-hover">About us</a>
+    <a className="link link-hover">Contact</a>
+    <a className="link link-hover">Jobs</a>
+    <a className="link link-hover">Press kit</a>
+  </nav>
+  <nav>
+    <h6 className="footer-title">Brittany Pizarro</h6>
+    <a className="link link-hover">Terms of use</a>
+    <a className="link link-hover">Privacy policy</a>
+    <a className="link link-hover">Cookie policy</a>
+  </nav>
+</footer>
+    )
+}
