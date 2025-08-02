2077321374c4815eb0f3acaf0baa3b85d142c22e
AFranklin30 <afrank25303@gmail.com>
2025-08-02 00:17:33 -0500

    updating flashcard display
    passing props into the flashcard display function.

--- flashcard/app/components/flashcard-display.js
+++ flashcard/app/components/flashcard-display.js
@@ -1,13 +1,13 @@
 
-export default function flashCardDisplay() {
+export default function flashCardDisplay({card, isFlipped, onFlip, onNext, onPrevious }) {
     return (
         <div>
             <h1>Spanish Flashcards</h1>
-              <div onClick={handleFlip}>{isFlipped ? 
-              cards[cardIndex].english : cards[cardIndex].spanish}
+              <div onClick={onFlip}>{isFlipped ? 
+              card[cardIndex].english : card[cardIndex].spanish}
                
                 </div>
-                <button onClick={OnNext}></button>        
+                <button onClick={onNext}></button>        
                 <button onClick={onPrevious}></button>        
                 <button onClick={OnNeedWork}></button>        
         
