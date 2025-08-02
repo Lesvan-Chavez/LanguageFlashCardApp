2cbf4d99233e690d62bc21738d1485d11290a83f
AFranklin30 <afrank25303@gmail.com>
2025-08-02 11:42:33 -0500

    flashcard container
    imported react into container

--- flashcard/app/components/flashcard-container.js
+++ flashcard/app/components/flashcard-container.js
@@ -1,5 +1,7 @@
+import React from "react";
 import { useState } from "react";
 import flashCardDisplay from "./flashcard-display";
+
 export default function FlashcardContainer () {
 
     const [cards, setCards] = useState([
