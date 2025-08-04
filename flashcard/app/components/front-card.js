80ee5191a031310f077f6c12a3cec8c7b42eab6f
AFranklin30 <afrank25303@gmail.com>
2025-08-04 00:46:47 -0500

    Update front-card.js
    updated utility classes.
    
    changed background color
    
    fixed minimum height

--- flashcard/app/components/front-card.js
+++ flashcard/app/components/front-card.js
@@ -3,8 +3,8 @@ import React from "react"
 
 export default function FrontCard({card, onFlip}) {
     return(
-        <div onClick={onFlip} className="bg-green-500 text-white p-8 rounded-lg shadow-lg cursor-pointer hover:bg-green-600 transition-colors duration-200 min-h-[200px] flex items-center justify-center">
-            <h2 text-3xl font-bold text-gray-800>{card.spanish}</h2>
+        <div onClick={onFlip} className="bg-white text-gray-800 p-8 rounded-xl shadow-lg cursor-pointer hover:bg-green-600 transition-colors duration-200 min-h-[300px] w-96 flex items-center justify-center border border-gray-200">
+            <h2 className= "text-3xl font-bold text-gray-800">{card.spanish}</h2>
 
         </div>
         
