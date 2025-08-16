6ddc77b4a94c53342b79606fd82c984036f7f763
Lesvan Chavez <lesvanrealm@gmail.com>
2025-08-16 13:06:40 -0500

    Updated Imports
    Updated Imports

--- flashcard/app/dashboard/page.jsx
+++ flashcard/app/dashboard/page.jsx
@@ -3,7 +3,7 @@
 import { useAuth } from "../../lib/context/AuthContext";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
-import FlashcardContainer from "../components/flashcard-container";
+import FlashcardContainer from "../components/card_components/flashcard-container";
 // import DeckPicker from "../components/deck-picker";
 // import LanguagePicker from "../components/language-picker";
 
